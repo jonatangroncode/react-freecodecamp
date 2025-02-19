12a2952bcf2df86be141e8368e1df423e325ded7
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-02-19 23:56:40 +0100

    style for navbar

--- src/components/Navbar.jsx
+++
@@ -1,5 +0,0 @@
-const Navbar = () => {
-  return <h1>Navbar component here</h1>;
-};
-
-export default Navbar;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="./src/assets/react.svg" alt="logo" />
+        <span>ReactFacts </span>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
