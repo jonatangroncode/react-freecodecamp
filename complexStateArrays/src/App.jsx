369370b9c7c8220004d378cb802b4e05ec4585db
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-14 23:35:56 +0100

    testing complex state handeling

---
+++ complexStateArrays/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ComplexState from "./components/complexstate/ComplexState";
+
+function App() {
+  return (
+    <>
+      <ComplexState />
+    </>
+  );
+}
+
+export default App;
