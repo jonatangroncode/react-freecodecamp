b5a506e43d6df2fa8474a0ba001cba45f9431eed
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-16 21:50:34 +0100

    changing from onSubmit to action on form

--- forms/src/components/forms/Forms.jsx
+++ forms/src/components/forms/Forms.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import "./Forms.css";
 const Forms = () => {
-  function handleSubmit(event) {
-    event.preventDefault();
+  function signUp(formData) {
     console.log("Form submitted");
-    const formData = new FormData(event.currentTarget);
     const email = formData.get("email");
     const password = formData.get("password");
     console.log(`Email: ${email}`);
     console.log(`Password: ${password}`);
-    event.currentTarget.reset();
   }
 
   return (
     <section>
       <h1>Signup Form</h1>
-      <form method="post" onSubmit={handleSubmit}>
+      <form action={signUp}>
         <label htmlFor="email">Email: </label>
         <input
           id="email"
