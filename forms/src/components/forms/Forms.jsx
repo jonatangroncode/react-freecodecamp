69fd2a74b63fd60a8cd203484189eae0fcd4d90b
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-03-16 21:15:37 +0100

    Added functionallity to test a post of the form

--- forms/src/components/forms/Forms.jsx
+++ forms/src/components/forms/Forms.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Forms.css";
 const Forms = () => {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Form submitted");
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+    event.currentTarget.reset();
+  }
+
   return (
     <section>
       <h1>Signup Form</h1>
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <label htmlFor="email">Email: </label>
         <input
           id="email"
@@ -14,7 +25,12 @@ const Forms = () => {
         />
         <br />
         <label htmlFor="password">Password: </label>
-        <input id="password" type="password" name="password" />
+        <input
+          id="password"
+          type="password"
+          name="password"
+          placeholder="password"
+        />
         <br />
         <button>Submit</button>
       </form>
