6d30918b18557d5c8a2680cc0b2d050ef2002651
jonatangroncode <jonatan.gron.code@hotmail.com>
2025-04-05 23:40:42 +0200

    conditional rendering

--- jokes/src/components/jokes/Jokes.jsx
+++ jokes/src/components/jokes/Jokes.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import { useState } from "react";
 
 const Jokes = (props) => {
+  const [isShown, setIsShown] = useState(false);
+
+  function togglePunchline() {
+    setIsShown((prev) => !prev);
+  }
+
+  console.log(isShown);
   console.log(props);
   return (
     <>
       {props.setup && <p>{props.setup}</p>}
-      <p>{props.punchline}</p>
+      {isShown === true && <p>{props.punchline}</p>}
+      <button onClick={togglePunchline}>
+        {isShown ? "Hide punchline" : "Show punchline"}
+      </button>
       <hr />
     </>
   );
